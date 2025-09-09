5de5569a836ce6818faa106166bea86b7517a65c
NoOne619 <abdullahmehmood454@gmail.com>
2025-09-09 18:07:55 +0500

    hello azure added

--- app.py
+++ app.py
@@ -44,6 +44,9 @@ def logout():
 @app.route("/about")
 def about():
     return render_template("about.html")
+@app.route("/hello")
+def hello():
+    return render_template("hello.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
